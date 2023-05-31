25739cc7a914dcf90ac0846af0fe7c12ffccc79e
Luka Yurii <lukayurii.work@gmail.com>
2023-05-31 21:29:15 +0500

    Setup logic getRed

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -12,18 +12,22 @@ class ViewController: UIViewController {
     @IBOutlet var redLable: UILabel!
     @IBOutlet var redSliderValue: UILabel!
     @IBOutlet var redSlider: UISlider!
-    @IBOutlet var VIewColor: UIView!
+    @IBOutlet var viewColor: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupSlider()
         redSliderValue.text = String(redSlider.value)
         
+        
     }
     
     @IBAction func redSliderAction() {
         redSliderValue.text = String(round(redSlider.value * 100) / 100)
+        viewColor.backgroundColor = changeColor()
     }
+    
     
     
     
@@ -32,5 +36,24 @@ class ViewController: UIViewController {
         redSlider.value = 0.15
         redSlider.minimumValue = 0.0
         redSlider.maximumValue = 1.0
+    }
+    
+    private func changeColor() -> UIColor {
+        UIColor(red: CGFloat(redSlider.value), green: 0.1, blue: 0.5, alpha: 1)
+    }
+}
+
+
+//MARK: Extension UIColor
+extension UIColor {
+    var rgba: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        
+        getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+
+        return (red, green, blue, alpha)
     }
 }
