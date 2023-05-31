2345a002e39d5c4b61953f1b473045ddc61c3805
Luka Yurii <lukayurii.work@gmail.com>
2023-05-31 20:23:20 +0500

    Setup Main View, red slider and label

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLable: UILabel!
+    @IBOutlet var redSliderValue: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var VIewColor: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupSlider()
+        redSliderValue.text = String(redSlider.value)
+        
     }
-
-
+    
+    @IBAction func redSliderAction() {
+        redSliderValue.text = String(round(redSlider.value * 100) / 100)
+    }
+    
+    
+    
+    //MARK: - Private Methods
+    private func setupSlider() {
+        redSlider.value = 0.15
+        redSlider.minimumValue = 0.0
+        redSlider.maximumValue = 1.0
+    }
 }
-
