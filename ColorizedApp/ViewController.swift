8c84cdbd95c285d2ec2ea04fe6f2d01eaf59e49b
Luka Yurii <lukayurii.work@gmail.com>
2023-05-31 21:50:04 +0500

    Adding and setup Green and Blue sliders

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -14,11 +14,22 @@ class ViewController: UIViewController {
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var viewColor: UIView!
     
+    @IBOutlet var greenLable: UILabel!
+    @IBOutlet var greenSliderValue: UILabel!
+    @IBOutlet var greenSlider: UISlider!
+    
+    @IBOutlet var blueLable: UILabel!
+    @IBOutlet var blueSliderValue: UILabel!
+    @IBOutlet var blueSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupSlider()
+        setupRedSlider()
+        setupGreenSlider()
+        setupBlueSlider()
         redSliderValue.text = String(redSlider.value)
+        greenSliderValue.text = String(greenSlider.value)
+        blueSliderValue.text = String(blueSlider.value)
         
         
     }
@@ -28,18 +39,38 @@ class ViewController: UIViewController {
         viewColor.backgroundColor = changeColor()
     }
     
+    @IBAction func greenSliderAction() {
+        greenSliderValue.text = String(round(greenSlider.value * 100) / 100)
+        viewColor.backgroundColor = changeColor()
+    }
     
+    @IBAction func blueSliderAction() {
+        blueSliderValue.text = String(round(blueSlider.value * 100) / 100)
+        viewColor.backgroundColor = changeColor()
+    }
     
     
     //MARK: - Private Methods
-    private func setupSlider() {
+    private func setupRedSlider() {
         redSlider.value = 0.15
         redSlider.minimumValue = 0.0
         redSlider.maximumValue = 1.0
     }
     
+    private func setupGreenSlider() {
+        greenSlider.value = 0.27
+        greenSlider.minimumValue = 0.0
+        greenSlider.maximumValue = 1.0
+    }
+    
+    private func setupBlueSlider() {
+        blueSlider.value = 0.47
+        blueSlider.minimumValue = 0.0
+        blueSlider.maximumValue = 1.0
+    }
+    
     private func changeColor() -> UIColor {
-        UIColor(red: CGFloat(redSlider.value), green: 0.1, blue: 0.5, alpha: 1)
+        UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
 }
 
